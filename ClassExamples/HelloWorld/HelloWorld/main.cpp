6a5036c5f27c582813b5efaa7e76995803b37ed5
erictarrbelmont <erictarrbelmont@gmail.com>
2021-01-15 09:56:20 -0600

    Added class examples folder

---
+++ ClassExamples/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Eric Tarr on 1/14/21.
+//  Copyright © 2021 Eric Tarr. All rights reserved.
+//
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    cout << "Hello, World!\n";
+
+    int a;
+    a = 3/2;
+    
+    int c = 6;
+    
+    float b;
+    b = 5.5f;
+    
+    double g;
+    g = 6.0;
+    
+    cout << a << endl;
+    
+    float x[5] = {0.0f};
+    x[0] = 12.0f;
+    x[3] = -6.2f;
+    
+    return 0;
+}
