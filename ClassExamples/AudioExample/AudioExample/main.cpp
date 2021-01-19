4b98cb61dffac0abff79d58dfb9c7fa6550be0d3
erictarrbelmont <erictarrbelmont@gmail.com>
2021-01-19 12:52:36 -0600

    Class prep for 1/19

---
+++ ClassExamples/AudioExample/AudioExample/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  AudioExample
+//
+//  Created by Hack Audio on 10/5/19.
+//  Copyright © 2019 Eric Tarr. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+#include "AudioFile/AudioFile.hpp"
+using namespace std;
+
+int main() {
+    
+    
+    return 0;
+}
