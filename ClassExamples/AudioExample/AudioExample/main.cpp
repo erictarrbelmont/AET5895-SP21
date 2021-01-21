e9f86902df144301b10b2be64e5a401de61c115d
erictarrbelmont <erictarrbelmont@gmail.com>
2021-01-21 13:25:51 -0600

    Progress from class 1/19

--- ClassExamples/AudioExample/AudioExample/main.cpp
+++ ClassExamples/AudioExample/AudioExample/main.cpp
@@ -11,8 +11,55 @@
 #include "AudioFile/AudioFile.hpp"
 using namespace std;
 
+// Declare our own function
+void dBAmpChange(vector<float> & x,float dB);
+
+
 int main() {
+    
+    // Import audio file
+    // [x,Fs] = audioread('AcGtr.wav');
+    string filename = "AcGtr.wav";
+    int Fs;
+    int bitDepth;
+    int numChannels;
+    vector<float> signal; // dynamic allocation of memory for array
+    
+    audioread(filename,signal,Fs,bitDepth,numChannels);
+    
+//    int N = signal.size();
+//    // Loop to change amplitude
+//    for (int n = 0 ; n < N ; n++ ){
+//        //signal[n] = signal[n] * 0.25f;
+//        signal[n] *= 0.25f;
+//    }
+    float dBAmp = 6.f;
+    dBAmpChange(signal,dBAmp);
+    
+        
+    // output processed signal
+    string outName = "Test.wav";
+    audiowrite(outName,signal,Fs,bitDepth,numChannels);
     
     
     return 0;
 }
+
+// Define the function (steps of processing)
+void dBAmpChange(vector<float> & x, float dB){
+    
+    //float linAmp = 10^(dB/20);
+    float linAmp = pow(10.f,dB/20.f);
+    
+    int N = x.size();
+    for (int n = 0 ; n < N ; n++){
+        x[n] *= linAmp;
+    }
+    
+    
+    
+}
+
+
+
+
